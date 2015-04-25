2da0595259337a5c9b98c5b54eb64315b651bbe0
hzouba <hfaidh.zoubair@gmail.com>
2015-04-25 14:54:15 +0100

    my first commit

---
+++ showsonly/showsonly-web/src/main/java/mBeans/LoginBean.java
@@ -0,0 +1,43 @@
+package mBeans;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import services.interfaces.AccountManagementLocal;
+import domain.Admin;
+import domain.User;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+	private User user = new User();
+	@EJB
+	private AccountManagementLocal accountManagementLocal;
+
+	public String doLogin() {
+		String navigateTo = "";
+		User userFound = accountManagementLocal.login(user.getLogin(),
+				user.getPassword());
+		if (userFound != null) {
+			if (userFound instanceof Admin) {
+				navigateTo = "/pages/admin/adminHome?faces-redirect=true";
+			} else {
+				navigateTo = "/pages/logged/loggedHome?faces-redirect=true";
+			}
+
+		} else {
+			navigateTo = "/error?faces-redirect=true";
+		}
+		return navigateTo;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
